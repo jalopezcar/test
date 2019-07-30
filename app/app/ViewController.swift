9cd7dd26497857232e9771ea8dbec225c9abcf9a
Juan Antonio López <jlopezcarreno@groupon.com>
2019-07-30 04:00:29 -0700

    init

---
+++ app/app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Juan Antonio Lopez Carreño on 7/30/19.
+//  Copyright © 2019 Juan Antonio Lopez Carreño. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
